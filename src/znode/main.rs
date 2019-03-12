1f0b24586b3ec69dc429e9e6c4fa7a27e7685c88
yosiSF <stephan.haller@v-l.live>
2019-03-12 15:59:50 -0700

    Main takes care of Config Check and DB Pool setup
    Inside Main we'll do a panic/err check on the listeners, and either crash the server or check username existence and crash servers or hangle database connection problems and terminate the server.

--- src/znode/main.rs
+++ src/znode/main.rs
@@ -1,14 +1,34 @@
+#![feature(never_type)]
+#![feature(proc_macro_hygiene, decl_macro)]
+extern crate chrono;
+extern crate crypto;
 #[macro_use]
 extern crate error_chain;
 #[macro_use]
+extern crate diesel;
+extern crate dotenv;
+extern crate env_logger;
+extern crate hex;
 extern crate serde_derive;
 extern crate bincode;
 #[macro_use]
 extern crate clap;
-
+extern crate hyper;
+extern crate hyper_native_tls;
 #[macro_use] extern crate log;
 extern crate env_logger;
-
+extern crate lazy_static;
+#[macro_use]
+extern crate rocket;
+extern crate rocket_contrib;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+#[macro_use]
+extern crate serde_json;
+extern crate toml;
+extern crate url;
+extern crate urlencoded;
 extern crate futures;
 extern crate rand;
 extern crate tokio_core;
@@ -21,6 +41,27 @@ use std::vec::Vec;
 mod errors;
 use errors::*;
 mod node;
+
+#[macro_use]
+extern crate maplit;
+extern crate itertools;
+#[macro_use]
+mod macros;
+mod config;
+mod domain;
+mod error;
+mod github;
+mod nag;
+mod scraper;
+mod server;
+mod teams;
+use chrono::Local;
+use diesel::pg::PgConnection;
+use diesel::r2d2::Pool;
+use diesel::r2d2::ConnectionManager;
+use env_logger::LogBuilder;
+use log::LogRecord;
+use config::CONFIG;
 
 quick_main!(run);
 
